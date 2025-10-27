a0aa74e7f87d0313aef3d9fb41a1e4fa2053f55f
jykim170 <vertoco@naver.com>
2025-10-27 11:43:48 +0900

    refactor(controller): 예외 처리 및 메서드 분리로 가독성 향상

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -11,12 +11,27 @@ import java.util.List;
 public class RacingController {
 
     public void run() {
-        List<String> names = Validators.parseNames(InputView.readNames());
-        int tryCount = Validators.parseTryCount(InputView.readTryCount());
+        try {
+            List<String> names = readCarNames();
+            int tryCount = readTryCount();
+            playGame(names, tryCount);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+        }
+    }
 
+    private List<String> readCarNames() {
+        return Validators.parseNames(InputView.readNames());
+    }
+
+    private int readTryCount() {
+        return Validators.parseTryCount(InputView.readTryCount());
+    }
+
+    private void playGame(List<String> names, int tryCount) {
+        RacingGame game = RacingGame.of(names, new RandomMoveStrategy());
         OutputView.printStart();
 
-        RacingGame game = RacingGame.of(names, new RandomMoveStrategy());
         for (int i = 0; i < tryCount; i++) {
             game.raceOnce();
             OutputView.printRound(game.snapshot());
